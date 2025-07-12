c71f996ca016f4e38d5d038a0cd8fafd3384cbbe
KASHYAPCT <kashyapchandran38@gamil.com>
2025-07-12 14:03:22 +0530

    emi edit and view

--- bepocart/urls.py
+++ bepocart/urls.py
@@ -7,6 +7,7 @@ from .views import *
 urlpatterns = [
     path('products/',ProductListView.as_view()),
     path('emi/', EmiView.as_view(), name='emi'),
+    path('loan/<int:pk>/', LoanDetailView.as_view(), name='loan-detail'),
     path('loan/emi/<int:loan_id>/', LoanEMIView.as_view(), name='loan-emi'),
     path('emiexpense/<int:emi_id>/', EmiExpenseView.as_view(), name='emi-expense-detail'),
     path('assests/get/',AssetsAPIView.as_view()),
