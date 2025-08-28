09502f31135acc70ed1b3dfb36d9f2e27c1c06e2
KASHYAPCT <kashyapchandran38@gamil.com>
2025-08-28 14:31:39 +0530

    new field added in DataLog model

--- beposoft_app/models.py
+++ beposoft_app/models.py
@@ -1369,6 +1369,7 @@ class DataLog(models.Model):
     Stores user (from token), before/after data snapshots, and timestamp.
     """
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='data_logs')
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True, related_name='data_logs')
     before_data = models.JSONField(null=True, blank=True, default=dict)
     after_data  = models.JSONField(null=True, blank=True, default=dict)
     created_at = models.DateTimeField(auto_now_add=True)
