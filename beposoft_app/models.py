30d9ca40b88c7907837b0cff0b3e6905612a73ca
KASHYAPCT <kashyapchandran38@gamil.com>
2025-12-16 12:24:28 +0530

    new field added in refundreceipt model

--- beposoft_app/models.py
+++ beposoft_app/models.py
@@ -1336,6 +1336,7 @@ class RefundReceipt(models.Model):
     date = models.DateField()
     note = models.CharField(max_length=50, null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="refund_created")
+    transactionID = models.CharField(max_length=50, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
