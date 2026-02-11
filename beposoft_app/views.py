b4f2d10f7a53c588c339d450a141fd98c955405b
KASHYAPCT <kashyapchandran38@gamil.com>
2026-02-11 10:17:28 +0530

    .

--- beposoft_app/views.py
+++ beposoft_app/views.py
@@ -6816,7 +6816,7 @@ class BankAccountTypeReportView(BaseTokenView):
                     if used_amount < 0:
                         used_amount = 0
 
-                    daily_interest = ((used_amount * rate) / 100 )/ 365
+                    daily_interest = ((used_amount * rate) / 100 )
 
                     total_interest += daily_interest
 
